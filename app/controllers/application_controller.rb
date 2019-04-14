722aad6e6bd8e10b8dcd0edda71cae6b2d59d488
Zachary Lehmann <zlehmann@gmail.com>
2019-04-14 19:41:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,7 @@ class ApplicationController < ActionController::Base
 
     def authenticate_user
       redirect_to "https://github.com/login/oauth/authorize?client_id=#{ENV['GITHUB_CLIENT']}&scope=repo" if !logged_in?
+      debugger
     end
 
     def logged_in?
