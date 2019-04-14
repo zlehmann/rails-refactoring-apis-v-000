8fab00785bea32a2a545a23bde8491699adec685
Zachary Lehmann <zlehmann@gmail.com>
2019-04-14 19:02:42 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,8 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate_user, only: :create
 
-
+  def create
+    github_repo = GithubRepo.new
+    session[:token] = github_repo.authenticate!(ENV['GITHUB_CLIENT']), ENV['GITHUB_SECRET'], params[:code])
+    redirect_to root_path
 end
