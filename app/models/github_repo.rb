bd039dd1125e7e544fab3f234ffbc71cc15e17c8
Zachary Lehmann <zlehmann@gmail.com>
2019-04-14 19:00:28 +0000

    Automatically backed up by Learn

--- app/models/github_repo.rb
+++ app/models/github_repo.rb
@@ -7,7 +7,7 @@ class GithubRepo
     @url = hash["html_url"]
   end
 
-  def create
+  def authenticate!(client_id, client_secret, code)
     response = Faraday.post "https://github.com/login/oauth/access_token", {client_id: ENV["GITHUB_CLIENT"], client_secret: ENV["GITHUB_SECRET"],code: params[:code]}, {'Accept' => 'application/json'}
     access_hash = JSON.parse(response.body)
     session[:token] = access_hash["access_token"]
@@ -16,7 +16,6 @@ class GithubRepo
     user_json = JSON.parse(user_response.body)
     session[:username] = user_json["login"]
 
-    debugger
     redirect_to '/'
   end
 
